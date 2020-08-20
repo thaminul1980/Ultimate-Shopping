035b9a85ea6716554eb8ccd40db05bd9d41df3df
Thaminul Islam <asifredhat@yahoo.com>
2020-08-20 16:05:39 +0600

    Successfully Loaded Static Resources

---
+++ UltimateShopping/src/main/java/com/example/UltimateShopping/controller/PageController.java
@@ -0,0 +1,22 @@
+package com.example.UltimateShopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/", "home", "index"})
+	
+	public ModelAndView index()  {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting" , "Welcome to Spring MVC");
+				
+				return mv;
+	}
+	
+	}
+
+
