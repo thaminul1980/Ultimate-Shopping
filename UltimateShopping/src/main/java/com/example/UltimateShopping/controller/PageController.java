125f732880e05591e952b6ed1b4f5d0c63b166e7
Thaminul Islam <asifredhat@yahoo.com>
2020-08-22 17:25:54 +0600

    completed the interface theme and other issues!

--- UltimateShopping/src/main/java/com/example/UltimateShopping/controller/PageController.java
+++ UltimateShopping/src/main/java/com/example/UltimateShopping/controller/PageController.java
@@ -7,15 +7,39 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PageController {
 	
-	@RequestMapping(value= {"/", "home", "index"})
+	@RequestMapping(value= {"/", "/home", "/index"})
 	
 	public ModelAndView index()  {
 		
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting" , "Welcome to Spring MVC");
-				
+		/* mv.addObject("greeting" , "Welcome to Spring MVC"); */
+		
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome", true);		
 				return mv;
 	}
+	
+	
+	
+        @RequestMapping(value= {"/about"})
+	
+	public ModelAndView about()  {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout", true);		
+				return mv;
+	}
+
+@RequestMapping(value= {"/contact"})
+
+public ModelAndView contact()  {
+	
+	ModelAndView mv = new ModelAndView("page");
+	mv.addObject("title","Contact Us");
+	mv.addObject("userClickContact", true);		
+			return mv;
+}
 	
 	}
 
---
+++ UltimateShopping/src/main/webapp/WEB-INF/views/shared/footer.jsp
@@ -0,0 +1,14 @@
+    <div class="container footer">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Ultimate Shopping 2020 - Designed and Developed By Thaminul Islam</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
