ebc6658474d6ddc3d79df1bf1d3fffc246368063
Thaminul Islam <asifredhat@yahoo.com>
2020-08-30 14:40:18 +0600

    final try

--- UltimateShopping/src/main/webapp/WEB-INF/views/listProducts.jsp
+++ UltimateShopping/src/main/webapp/WEB-INF/views/listProducts.jsp
@@ -2,54 +2,112 @@
 
 	<div class="row">
 
-		<!-- would be display sidebar -->
 
+		<!-- Would be to display sidebar -->
 		<div class="col-md-3">
+
 			<%@include file="./shared/sidebar.jsp"%>
+
 		</div>
 
-		<!-- To display actual bar -->
-
+		<!-- to display the actual products -->
 		<div class="col-md-9">
 
-			<!-- added breadcrumb component -->
-
+			<!-- Added breadcrumb component -->
 			<div class="row">
 
 				<div class="col-lg-12">
 
-					<c:if test="${userClickAllProducts == true }">
-
+					<c:if test="${userClickAllProducts == true}">
+					
+						<script>
+							window.categoryId = '';
+						</script>
+					
 						<ol class="breadcrumb">
 
-							<li class="home"><a class="nav-link"
-								href="${contextRoot}/home">Home</a></li>
+
+							<li><a href="${contextRoot}/home">Home</a></li>
 							<li class="active">All Products</li>
 
+
 						</ol>
-
 					</c:if>
-
-					<c:if test="${userClickCategoryProducts == true }">
-
+					
+					
+					<c:if test="${userClickCategoryProducts == true}">
+						<script>
+							window.categoryId = '${category.id}';
+						</script>
+					
 						<ol class="breadcrumb">
 
-							<li class="home"><a class="nav-link"
-								href="${contextRoot}/home">Home</a></li>
+
+							<li><a href="${contextRoot}/home">Home</a></li>
 							<li class="active">Category</li>
 							<li class="active">${category.name}</li>
 
-						</ol>
 
+						</ol>
 					</c:if>
+					
 
 				</div>
 
+
 			</div>
+
+			
+			<div class="row">
+			
+				<div class="col-xs-12">
+				
+					
+					<table id="productListTable" class="table table-striped table-borderd">
+					
+					
+						<thead>
+						
+							<tr>
+								<th></th>
+								<th>Name</th>
+								<th>Brand</th>
+								<th>Price</th>
+								<th>Qty. Available</th>
+								<th></th>
+							
+							</tr>
+						
+						</thead>
+					
+
+						<tfoot>
+						
+							<tr>
+								<th></th>
+								<th>Name</th>
+								<th>Brand</th>
+								<th>Price</th>
+								<th>Qty. Available</th>
+								<th></th>
+							
+							</tr>
+						
+						</tfoot>
+					</table>
+				
+				</div>
+			
+			</div>
+
 
 		</div>
 
+
+
 	</div>
+
+
 
 
 
---
+++ UltimateShoppingBackend/src/test/java/com/example/UltimateShoppingBankend/test/CartLineTestCase.java
@@ -0,0 +1,96 @@
+package com.example.UltimateShoppingBankend.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.UltimateShoppingBackend.CartLineDAO;
+import com.example.UltimateShoppingBackend.dao.ProductDAO;
+import com.example.UltimateShoppingBackend.dao.UserDAO;
+import com.example.UltimateShoppingBackend.dto.Cart;
+import com.example.UltimateShoppingBackend.dto.CartLine;
+import com.example.UltimateShoppingBackend.dto.User;
+
+public class CartLineTestCase {
+
+	
+
+	private static AnnotationConfigApplicationContext context;
+	
+	
+	private static CartLineDAO cartLineDAO;
+	private static ProductDAO productDAO;
+	private static UserDAO userDAO;
+	
+	
+	private CartLine cartLine = null;
+	
+	
+	@BeforeClass
+	public static void init() {
+		context = new AnnotationConfigApplicationContext();
+		context.scan("net.kzn.shoppingbackend");
+		context.refresh();
+		cartLineDAO = (CartLineDAO)context.getBean("cartLineDAO");
+		productDAO = (ProductDAO)context.getBean("productDAO");
+		userDAO = (UserDAO)context.getBean("userDAO");
+	}
+	
+/*	
+	@Test
+	public void testAddCartLine() {
+		
+		// fetch the user and then cart of that user
+		User user = userDAO.getByEmail("absr@gmail.com");		
+		Cart cart = user.getCart();
+		
+		// fetch the product 
+		Product product = productDAO.get(2);
+		
+		// Create a new CartLine
+		cartLine = new CartLine();
+		cartLine.setCartId(cart.getId());
+		cartLine.setProduct(product);
+		cartLine.setProductCount(1);
+		
+		double oldTotal = cartLine.getTotal();		
+		
+		cartLine.setTotal(product.getUnitPrice() * cartLine.getProductCount());
+		
+		cart.setCartLines(cart.getCartLines() + 1);
+		cart.setGrandTotal(cart.getGrandTotal() + (cartLine.getTotal() - oldTotal));
+		
+		assertEquals("Failed to add the CartLine!",true, cartLineDAO.add(cartLine));
+		assertEquals("Failed to update the cart!",true, userDAO.updateCart(cart));
+		
+	}
+	
+	*/
+	
+	@Test
+	public void testUpdateCartLine() {
+
+		// fetch the user and then cart of that user
+		User user = userDAO.getByEmail("absr@gmail.com");		
+		Cart cart = user.getCart();
+				
+		cartLine = cartLineDAO.getByCartAndProduct(cart.getId(), 2);
+		
+		cartLine.setProductCount(cartLine.getProductCount() + 1);
+				
+		double oldTotal = cartLine.getTotal();
+				
+		cartLine.setTotal(cartLine.getProduct().getUnitPrice() * cartLine.getProductCount());
+		
+		cart.setGrandTotal(cart.getGrandTotal() + (cartLine.getTotal() - oldTotal));
+		
+		assertEquals("Failed to update the CartLine!",true, cartLineDAO.update(cartLine));	
+
+		
+	}
+	
+	
+	
+}
